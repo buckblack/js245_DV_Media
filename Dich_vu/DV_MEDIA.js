39ec71ce13cd09b84769f47cc4c0f1be932456b2
buckblack <42344854+buckblack@users.noreply.github.com>
2019-03-15 19:29:53 +0700

    .

--- Dich_vu/DV_MEDIA.js
+++ Dich_vu/DV_MEDIA.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var Luu_tru = require("../Xu_ly/XL_LUU_TRU")
 var Xu_ly_Tham_so = require('querystring')
-var Port = 1001
+var Port = normalizePort(process.env.PORT || 1001)
 var Dich_vu = http.createServer(
     (Yeu_cau, Dap_ung) => {
         var Chuoi_Nhan = ""
@@ -11,16 +11,26 @@ var Dich_vu = http.createServer(
             .url
             .replace("/", "")
 
-        Yeu_cau.on('data', (chunk) => { Chuoi_Nhan += chunk })
+        Yeu_cau.on('data', (chunk) => {
+            Chuoi_Nhan += chunk
+        })
         Yeu_cau.on('end', () => {
             if (Yeu_cau.method == "GET" && Ten != "") {
                 Nhi_phan_Kq = Luu_tru.Doc_Nhi_phan_Media(Ten)
                 Dap_ung.setHeader("Access-Control-Allow-Origin", '*')
-                Dap_ung.writeHead(200, { 'Content-Type': 'image/png' });
+                Dap_ung.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+                Dap_ung.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+                Dap_ung.setHeader('Access-Control-Allow-Credentials', true);
+                Dap_ung.writeHead(200, {
+                    'Content-Type': 'image/png'
+                });
                 Dap_ung.end(Nhi_phan_Kq, 'binary');
-            }  else {
+            } else {
                 Chuoi_Kq = Luu_tru.Thong_tin_Dich_vu()
                 Dap_ung.setHeader("Access-Control-Allow-Origin", '*')
+                Dap_ung.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+                Dap_ung.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+                Dap_ung.setHeader('Access-Control-Allow-Credentials', true);
                 Dap_ung.end(Chuoi_Kq);
             }
 
@@ -28,6 +38,54 @@ var Dich_vu = http.createServer(
 
     })
 
-Dich_vu.listen(Port,
-    console.log("Dịch vụ Media đang thực thi ...: " + Port)
-)
+Dich_vu.listen(Port);
+Dich_vu.on('error', onError);
+Dich_vu.on('listening', onListening);
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof Port === 'string' ?
+        'Pipe ' + Port :
+        'Port ' + Port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    var addr = Dich_vu.address();
+    var bind = typeof addr === 'string' ?
+        'pipe ' + addr :
+        'port ' + addr.port;
+    console.log('Listening on ' + bind);
+}
